884c55d84a0f50186b9270c948c9809689a1d038
saeed81 <sd.falahat@gmail.com>
2019-04-14 11:17:07 +0200

    main is added

---
+++ CDCN/main.c
@@ -0,0 +1,14 @@
+#include"dcn.c"
+/*
+  !!----------------------------------------------------------------------     
+  !!                    ***  PROGRAM model  ***                               
+  !!   !! ** Purpose :   encapsulate the dcn model  
+  !!----------------------------------------------------------------------
+*/
+
+int main(int argc, char *argv[]){
+
+  dcn_model(argc,argv);
+
+  return 0;
+}
